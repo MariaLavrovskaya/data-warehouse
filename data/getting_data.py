f7ce74b3e0ea8f9719d74799801157f195954782
Maria Lavrovskaya <marialavrovs@gmail.com>
2021-09-23 17:56:12 +0100

    modified

--- data/getting_data.py
+++ data/getting_data.py
@@ -6,7 +6,7 @@ import pandas as pd
 pairs = ['BTC-USD', 'ETH-USD', 'HEX-USD', 'ADA-USD', 'USDT-USD', 'BNB-USD', 'XRP-USD', 'SOL1-USD', 'DOT1-USD', 'USDC-USD']
 tickers = yf.Tickers('BTC-USD ETH-USD HEX-USD ADA-USD USDT-USD BNB-USD XRP-USD SOL1-USD DOT1-USD USDC-USD')
 info_table = {pair: tickers.tickers[pair].info['name'] for pair in pairs}
-info_dataframe = pd.DataFrame(data = {'abbr': info_table.keys(), 'name': info_table.values()})
+info_dataframe = pd.DataFrame(data = {'abbr': info_table.keys(), 'name': info_table.values()}) #take only abbrevation and name from all available info
 info_dataframe.to_csv('information.csv', index=False)
 
 # getting  historical data
