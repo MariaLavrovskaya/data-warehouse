bfd31d487b59dc0e0d6154daa710d6a235a674ff
Maria Lavrovskaya <marialavrovs@gmail.com>
2021-09-24 10:51:30 +0100

    create database updated

--- connect.py
+++ connect.py
@@ -1,4 +1,5 @@
 import psycopg2
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 import os
 
 def load_database_credentials(db_info):
@@ -18,11 +19,29 @@ def load_database_credentials(db_info):
     lines = f.readlines()[1:][0].split(',')
     return lines 
 
+def create_database(db_info):
+    db_port, db_name, db_user, password = load_database_credentials(db_info)
+    print('Database is being created')
+    conn = psycopg2.connect(port=db_port, dbname='postgres', user=db_user, password=password)
+    conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+    cur = conn.cursor()
+    cur.execute('CREATE DATABASE %s ;' %db_name)
+    cur.close()
+
+
+
 def main():
     db_info = 'database_info.txt'
     db_port, db_name, db_user, password = load_database_credentials(db_info)
-    conn = psycopg2.connect(port=db_port, dbname=db_name, user=db_name, password=password)
-    cur = conn.cursor()
+    # check whether the database exists; if not, create the database 
+    try: 
+        conn = psycopg2.connect(port=db_port, dbname=db_name, user=db_user, password=password)
+        print('Database exists')
+    # cur = conn.cursor()
+    except:
+        create_database(db_info)
+    
+    
     # test
     # cur.execute("CREATE TABLE test (id serial, num integer, data varchar);")
     # conn.commit()
