d4dc9a13789e04bad1397f7ba7991b36110bdc5f
Maria Lavrovskaya <marialavrovs@gmail.com>
2021-09-28 14:40:06 +0100

    added queries

--- sql-queries.sql
+++ sql-queries.sql
@@ -1,33 +1,33 @@
 -- This file includes a number of SQL commands 
 
 
--- Print volumes for bitcoin and ethereum on  2018-01-01
+-- 1. Print volumes for bitcoin and ethereum on  2018-01-01
 SELECT b.volume, e.volume FROM bitcoin b full outer join  ethereum e on b.date=e.date where (b.date='2018-01-01' and e.date= '2018-01-01');
 
--- Create a temporary table that will hold information of the volumes of all the currency pairs on 2018-01-01
+-- 2. Create a temporary table that will hold information of the volumes of all the currency pairs on 2018-01-01
 create temp table if not exists temp_volumes (
     index int GENERATED ALWAYS AS IDENTITY,
     Date date,
     Volumes NUMERIC
 );
 
--- Fill the following table with the data of the volumes of all the currency pairs on 2018-01-01
+-- 3. Fill the following table with the data of the volumes of all the currency pairs on 2018-01-01
 INSERT INTO temp_volumes (Date, Volumes)
 SELECT b.date, b.volume
 FROM bitcoin b
 WHERE date='2018-01-01';
 
---Count the number of entries in the temporary table 
+--4. Count the number of entries in the temporary table 
 SELECT count(*) FROM temp_volumes;
 
---Count the number of volumes in the bitcoin table that has the count greater than two. Since this is done purely for education purposes this query reveals whether  there are any duplicates
+--5. Count the number of volumes in the bitcoin table that has the count greater than two. Since this is done purely for education purposes this query reveals whether  there are any duplicates
 SELECT volume, count(*) AS volume_count FROM bitcoin GROUP BY volume HAVING count(*)>2;
 
--- We now want to combine resultsets of two SELECT statements. Recall that UNION combines the values of the two tables and removes duplicates. If we do not want duplicates to be removed, we can use UNION ALL (really slow)
+-- 6. We now want to combine resultsets of two SELECT statements. Recall that UNION combines the values of the two tables and removes duplicates. If we do not want duplicates to be removed, we can use UNION ALL (really slow)
 SELECT volume FROM bitcoin UNION SELECT volume FROM ethereum;
 
 
------ One query
+----- 7. One query
 --Now we want to update the existing record with the new data -- say, we want to add record for the 2021-27-09 if it does not exist. To do that, we use UPSERT statement, which is a combination of INSERT and UPDATE. 
 --We do not want to insert info if the date with the record already exists, thus we use ON CONFLICT clause (ON CONFLICT TARGET ACTION). 
 INSERT INTO bitcoin(date, volume) VALUES('2021-09-27', '31013653888') ON CONFLICT DO NOTHING; -- Option 1. This does not update the row if the row already exists
@@ -39,4 +39,17 @@ and not id in (SELECT min(id) from bitcoin group by date); -- Rows that we want 
 
 --Then we add primary key so we have one unique constraint
 ALTER TABLE bitcoin ADD CONSTRAINT bitcoin_date_pkey PRIMARY KEY (date);
+
+
+-- 8. Alter some of the entries in the bitcoin table 
+-- First we create a temporary table 
+CREATE temp table t_bitcoin  AS SELECT * FROM bitcoin;
+-- Then we want to alter volume column to show the volume and then 'last updated + current_date'
+-- Since volume is of type numeric and we want to add some text first, we need to cast the types. We update the type of the column
+ALTER TABLE t_bitcoin ALTER COLUMN volume TYPE TEXT; 
+-- We then update the record 
+UPDATE t_bitcoin t SET volume = t.volume || 'last updated ' || current_date::text;
+
+
+
 
